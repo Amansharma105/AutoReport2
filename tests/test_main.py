1f5ba7f0501fea353c942e694509ac9524f03eac
Amansharma105 <sharmaaman87600@gmail.com>
2026-07-01 16:55:44 +0530

    Add initial test cases for application components

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+def test_app():
+    assert True
+
+def test_data_loader():
+    assert True
+
+def test_analysis():
+    assert True
+
+def test_chart_generator():
+    assert True
+
+def test_pdf_report():
+    assert True
