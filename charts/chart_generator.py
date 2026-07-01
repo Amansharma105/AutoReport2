08abb1fb2042ef499c37319ff91a8ad024de464f
Amansharma105 <sharmaaman87600@gmail.com>
2026-07-01 16:49:00 +0530

    Add charting functions for various chart types

--- charts/chart_generator.py
+++ charts/chart_generator.py
@@ -1,1 +1,17 @@
+import matplotlib.pyplot as plt
 
+def bar_chart(x, y):
+    plt.bar(x, y)
+    plt.title("Bar Chart")
+
+def line_chart(x, y):
+    plt.plot(x, y)
+    plt.title("Line Chart")
+
+def pie_chart(labels, values):
+    plt.pie(values, labels=labels, autopct="%1.1f%%")
+    plt.title("Pie Chart")
+
+def scatter_chart(x, y):
+    plt.scatter(x, y)
+    plt.title("Scatter Plot")
